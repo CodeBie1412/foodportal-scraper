9d9403f70d53a043b66760f404b1372feb090c8c
CodeBie1412 <45204179+CodeBie1412@users.noreply.github.com>
2024-07-09 21:15:49 +0800

    Fetch all titles

--- services/scraperService.js
+++ services/scraperService.js
@@ -30,72 +30,39 @@ async function scrape(link) {
 
 
 async function scrapeWebsite(link){
+  puppeteerextra.use(StealthPlugin());
 
+  const browser = await puppeteerextra.launch({headless: false});
 
-  puppeteerextra.use(StealthPlugin);
-
-  const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
 
-  await page.goto(link,{ waitUntil: 'networkidle2'});
+  try{
 
-  // const pageHTML = await page.content();
-  // console.log('page html:',pageHTML);
+    await page.goto(link, {waitUntil: 'networkidle2'});
 
+    // await page.screenshot({path: '/screenshot/screenshot.png', fullPage: true});
 
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    const frames = page.frames();
+    const allTitles = [];
 
-  // await page.waitForSelector('h2.dish-category-title', {visible:true});
-
-  const frames = page.frames();
-  const allTitles = [];
-  let titleDiv = null;
-
-  for (const frame of frames){
-    titleDiv = await frame.$$('h2.dish-category-title');
-
-    for (const titleDiv of TitleDivs){
-      const innerTeXT = 
+    for(const frame of frames){
+      const titleDivs = await frame.$$('h2.dish-category-title');
+      for(const titleDiv of titleDivs){
+        const innerText = await frame.evaluate(element=> element.innerText,titleDiv);
+        allTitles.push(innerText.trim());
+      }
     }
 
-    if (titleDiv){
-      logToFile('Inside iframe', 'app.log', {titleDiv});
-      break;
-    }
+    logToFile('Collected titles','app.log',{allTitles});
+    console.log('Scraped titles:', allTitles);
+    return allTitles;
+
+  } catch (error){
+    console.error('Error:', error);
+    await browser.close();
+    return [];
   }
-
-
-  asdadasds
-
-  const data = await page.evaluate(()=>{
-    const titleDiv = document.querySelector('h2.dish-category-title');
-
-    if(!titleDiv){
-      console.log('title div not found');
-      return null;
-    }
-
-    // logToFile('Reached here', 'app.log', {titleDiv});
-    console.log("Reached here");
-
-    const innerContain = titleDiv.innerText + "Test";
-    return innerContain;
-  });
-
-  // let data = null;
-
-  // if(titleDiv){
-  //   const innerContain = await frame.evaluate(element => element.innerText, titleDiv);
-  //   data = innerContain.trim();
-  // }
-  // else
-  // {
-  //   console.log('Title div not found');
-  // }
-
-  await browser.close();
-  return data
-
+ 
 }
 
 module.exports = { 
