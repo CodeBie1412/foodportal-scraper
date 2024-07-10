5621688ffab2513c724b2739fab25b5380212e0d
CodeBie1412 <45204179+CodeBie1412@users.noreply.github.com>
2024-07-10 11:19:35 +0800

    Enhance scriptd flow

--- services/scraperService.js
+++ services/scraperService.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer");
 const puppeteerextra = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const { logToFile } = require('../utils/logger');
+const fs = require('fs');
 
 
 async function scrape(link) {
@@ -49,7 +50,19 @@ async function scrapeWebsite(link){
       const titleDivs = await frame.$$('h2.dish-category-title');
       for(const titleDiv of titleDivs){
         const innerText = await frame.evaluate(element=> element.innerText,titleDiv);
-        allTitles.push(innerText.trim());
+
+        const spanData = await frame.evaluate(()=>{
+          const spanElement = document.querySelector('span.vertical-align-middle[data-testid="menu-product-name]');
+          return spanElement ? spanElement.innerText.trim() : null;
+        });
+
+        if (spanData){
+          console.log('Found span with data-testid="menu-product-name":', spanData);
+        }
+
+        allTitles.push({title: innerText.trim(),
+          spanData: spanData
+        });
       }
     }
 
@@ -65,7 +78,54 @@ async function scrapeWebsite(link){
  
 }
 
+async function scrapeWebsite2(link){
+  puppeteerextra.use(StealthPlugin());
+
+  const browser = await puppeteerextra.launch({headless: false});
+
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(link, { waitUntil: 'networkidle2' });
+
+    const data = await page.evaluate(() => {
+      const elements = document.querySelectorAll('div.box-flex.dish-category-section');
+      return Array.from(elements).map(element => element.innerText.trim());
+    });
+
+    const structuredData = data.map(section => {
+      const lines = section.split('\n').filter(line => line.trim() !== '');
+      const category = lines.shift();
+      const description = lines.shift();
+      const items = [];
+
+      while (lines.length){
+        const name = lines.shift();
+        const price = lines.shift();
+        const popularity = lines.shift();
+
+        items.push({name, price, popularity});
+      }
+
+      return { category, description, items};
+    });
+
+    console.log('Scraped Data:', structuredData);
+
+    fs.writeFileSync('scraped_data.json', JSON.stringify(structuredData, null, 2));
+
+    await browser.close();
+    return structuredData;
+  } catch (error) {
+    console.error('Error:', error);
+    await browser.close();
+    return [];
+  }
+  
+}
+
 module.exports = { 
   scrape,
-  scrapeWebsite
+  scrapeWebsite,
+  scrapeWebsite2
 };
